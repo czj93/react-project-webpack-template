a1bd678d60d90488310907461c561e48970adee4
zhijian <m15620830142@163.com>
2019-03-03 13:32:50 +0800

    更新懒加载库

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import loadable from '@loadable/component'
-// import pMinDelay from 'p-min-delay'
-import { Route,Switch,withRouter } from 'react-router-dom'
-import pMinDelay from '../lib/DelayComponent'
+import { Route, Switch } from 'react-router-dom'
+import Loadable from 'react-loadable';
+
 
 import { Spin } from 'antd'
 
@@ -11,18 +10,18 @@ import Home from '../pages/Home/'
 
 
 
-const LoadableTodoList = loadable(() => pMinDelay(import('../pages/TodoList/'),250),{ fallback: <Spin /> })
-const LoadableUser = loadable(() => pMinDelay(import('../pages/User/'),250),{ fallback: <Spin /> })
+const LoadableTodoList = Loadable({ loader:() => import('../pages/TodoList/'), loading: () => <Spin /> })
+const LoadableUser = Loadable({ loader:() => import('../pages/User/'), loading: () => <Spin /> })
 
 
-// var LoadableTodoList = loadable(() => import('../pages/TodoList/'),{ fallback: <Spin /> })
+
 class Router extends React.Component {
     render(){
         return (
             <Switch>
                 <Route exact path='/home' component={Home}></Route>
-                <Route exact path="/todolist" component={(props) => <LoadableTodoList {...props} />} ></Route> 
-                <Route exact path="/user/:id" component={(props) => <LoadableUser {...props} />} ></Route> 
+                <Route exact path="/todolist" component={LoadableTodoList} ></Route> 
+                <Route exact path="/user/:id" component={LoadableUser} ></Route> 
             </Switch>
         )
     }
